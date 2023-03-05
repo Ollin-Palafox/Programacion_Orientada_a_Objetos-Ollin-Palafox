4980951eb231f861443abe9881c0e5990981f170
ollin <ollinpmc@gmail.com>
2023-03-04 20:45:27 -0600

    Reto 5

---
+++ RC/edu/ollin/palafox/reto5/Main.java
@@ -0,0 +1,12 @@
+package edu.ollin.palafox.reto5;
+import edu.ollin.palafox.reto5.ui.CLI;
+
+/** En esta parte corremos nuestro codigo almacenado en CLI y lo traemos aqui para ejecutarlo
+ */
+public class Main {
+    //con el public static void main(String[]args) { CLI.MuestraElMenu(); } lo mandamos a traer y estara listo para
+    // ejecutarse
+    public static void main(String[]args){
+        CLI.MuestraElMenu();
+    }
+}
